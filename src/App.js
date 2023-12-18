f89dcb68bb53106a92fe63bbee3cf375e3457e3c
Hashem Almubarak <105522749+Hashim11Mub@users.noreply.github.com>
2023-12-18 19:23:40 +0300

    2 days of work

--- src/App.js
+++ src/App.js
@@ -1,29 +1,77 @@
+import React, { Component } from 'react';
 import './App.css';
+import ListItems from './listItems.js';
+import {library} from "@fortawesome/fontawesome-free";
+import {faTrash} from "@fortawesome/free-solid-svg-icons";
 
-import React, { Component } from 'react'
+library.add(faTrash)
 
-export class App extends Component {
+class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   
     this.state = {
-       items: [],
-       currentItem: {},
-       text: "",
-       Key: ''
+      items: [],
+      currentItem: {
+        text: "",
+        key: ""
+      }
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this)
+  }
+
+  handleInput(e) {
+    const { value } = e.target;
+    this.setState(prevState => ({
+      currentItem: {
+        text: value,
+        key: Date.now()
+      }
+    }));
+  }
+
+  addItem(e) {
+    e.preventDefault();
+    const { currentItem, items } = this.state;
+    if (currentItem.text !== "") {
+      const newItems = [...items, currentItem];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      });
     }
+  }
+
+  deleteItem(key) {
+    const filterItems = this.state.items.filter(item => 
+      item.key !== key );
+      this.setState({
+        items: filterItems
+      })
   }
   render() {
     return (
       <div className='App'>
       <header>
-        <form id= "to-do-form">
-          <input type="text" placeholder='Enter Message !!'/>
+        <form id= "to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder='Enter Message !!'
+          value={this.state.currentItem.text}
+          onChange={this.handleInput}
+          />
           <button type='Submit'>
               Add
           </button>
         </form>
       </header>
+      <ListItems items = {this.state.items} 
+      deleteItem={this.deleteItem}
+      />
       </div>
     )
   }
